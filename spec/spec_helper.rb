7f671a87aadeb923aadeb427b1064cd2eb008716
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-10 10:03:13 +0200

    Enable verifying doubles in RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'stateoscope'
+
+require 'timecop'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+    mocks.verify_partial_doubles = true
+  end
+end
