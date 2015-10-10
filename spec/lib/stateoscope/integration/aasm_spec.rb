57f58ceacd23bbb46aab01d0e174da33e219a145
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-10 10:21:50 +0200

    Add pending specs

---
+++ spec/lib/stateoscope/integration/aasm_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe Stateoscope::Integration::AASM do
+  pending
+end
---
+++ spec/lib/stateoscope/integration/base_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe Stateoscope::Integration::Base do
+  pending
+end
