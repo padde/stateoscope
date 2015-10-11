4bcbaa0021ce31e026b6ad6b6ee58e03bfbd43e6
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-11 11:54:01 +0200

    Make integration detection safer

--- lib/stateoscope/integration.rb
+++ lib/stateoscope/integration.rb
@@ -1,16 +1,21 @@
 require 'stateoscope/graph'
 require 'stateoscope/integration/base'
 
-require 'stateoscope/integration/aasm'
+require 'stateoscope/integration/aasm' if defined?(::AASM)
 
 module Stateoscope
   module Integration
     def self.new_for(klass, state_machine_name)
-      if klass.ancestors.include?(::AASM)
+      if class_inherits_from?(klass, '::AASM')
         ::Stateoscope::Integration::AASM.new(klass, state_machine_name)
       else
         fail MissingIntegrationError, 'unsupported state machine implementation'
       end
+    end
+
+    def self.class_inherits_from?(klass, superclass_name)
+      return false unless const_defined?(superclass_name)
+      klass.ancestors.include?(const_get(superclass_name))
     end
   end
 end
