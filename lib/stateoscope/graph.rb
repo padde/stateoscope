48ca0930360bf38d166d52d1d43d7b8c139f86b1
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-12 08:42:16 +0200

    Use accessors consistently

--- lib/stateoscope/graph.rb
+++ lib/stateoscope/graph.rb
@@ -5,25 +5,25 @@ module Stateoscope
     attr_accessor :initial_state, :states, :final_states, :transitions
 
     def initialize
-      @states = []
-      @final_states = []
-      @transitions = []
+      self.states = []
+      self.final_states = []
+      self.transitions = []
     end
 
     def add_state(state)
-      @states << state
+      states << state
     end
 
     def add_transition(from, to, event = nil)
-      @transitions << OpenStruct.new(from: from, to: to, event: event)
+      transitions << OpenStruct.new(from: from, to: to, event: event)
     end
 
     def detect_final_states!
-      @final_states = states - transitions.map(&:from)
+      self.final_states = states - transitions.map(&:from)
     end
 
     def final_state?(state)
-      @final_states.include?(state)
+      final_states.include?(state)
     end
   end
 end
