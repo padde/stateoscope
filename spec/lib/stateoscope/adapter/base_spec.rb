56e1d2aa48ca6dad32b810d4b8f1316227603ddb
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-18 14:50:28 +0200

    Add specs for Adapter::Base

--- spec/lib/stateoscope/adapter/base_spec.rb
+++ spec/lib/stateoscope/adapter/base_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 RSpec.describe Stateoscope::Adapter::Base do
-  pending
+  describe '.handle?' do
+    subject { described_class }
+    it { is_expected.to have_abstract_method(:handle?) }
+  end
+
+  describe '#initialize' do
+    it 'initializes a graph' do
+      adapter = described_class.new
+      expect(adapter.graph).to be_a(Stateoscope::Graph)
+    end
+  end
+
+  describe '#build_graph' do
+    it { is_expected.to have_abstract_method(:build_graph) }
+  end
+
+  describe '#full_state_machine_name' do
+    it { is_expected.to have_abstract_method(:full_state_machine_name) }
+  end
 end
