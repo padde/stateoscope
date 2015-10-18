a08a9ece94d367bb26a8df64095cb084e8e939f9
Patrick Oscity <patrick.oscity@gmail.com>
2015-10-18 16:49:01 +0200

    Add spec for Adapter

--- spec/lib/stateoscope/adapter_spec.rb
+++ spec/lib/stateoscope/adapter_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 RSpec.describe Stateoscope::Adapter do
-  pending
+  around do |example|
+    old = described_class.registry
+    described_class.registry = Stateoscope::AdapterRegistry.new
+    example.call
+    described_class.registry = old
+  end
+
+  describe '.register' do
+    it 'registers the given adapter with the registry' do
+      adapter = double(Stateoscope::Adapter::Base)
+      expect(described_class.registry).to receive(:register).with(adapter)
+      described_class.register(adapter)
+    end
+  end
+
+  describe '.new_for' do
+    it 'finds and instantiates an adapter from the registry' do
+      klass = Class.new
+      state_machine_name = :foo
+
+      wrong_adapter = class_double(Stateoscope::Adapter::Base, new: :wrong)
+      expect(wrong_adapter).to receive(:handle?).with(klass, state_machine_name).and_return(false)
+      described_class.register(wrong_adapter)
+
+      correct_adapter = class_double(Stateoscope::Adapter::Base, new: :correct)
+      expect(correct_adapter).to receive(:handle?).with(klass, state_machine_name).and_return(true)
+      described_class.register(correct_adapter)
+
+      expect(described_class.new_for(klass, state_machine_name)).to eq(:correct)
+    end
+  end
 end
